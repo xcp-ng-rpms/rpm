8e7ff61223533dad9171d95d570ff227f7783922
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    rpm-4.13.x-Make-the-stftime-buffer-big-enuff.patch

--- lib/formats.c
+++ lib/formats.c
@@ -101,7 +101,7 @@ static char * realDateFormat(rpmtd td, const char * strftimeFormat)
 	val = xstrdup(_("(not a number)"));
     } else {
 	struct tm * tstruct;
-	char buf[50];
+	char buf[1024];
 	time_t dateint = rpmtdGetNumber(td);
 	tstruct = localtime(&dateint);
 
